836617032e1c433b007a345a9d96792dfe79bf23
Gicu <gicurata@gmail.com>
2024-05-14 23:05:45 +0200

    Images parcing implemented

---
+++ Models/ImgurOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace ImageProcessingTool.Models
+{
+    public class ImgurOptions
+    {
+        public string ClientId { get; set; }
+        // Add other properties as needed...
+    }
+
+}
---
+++ Services/ImgurService.cs
@@ -0,0 +1,53 @@
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ImageProcessingTool.Models;
+using Microsoft.Extensions.Options;
+
+public class ImgurService
+{
+    private readonly HttpClient _client;
+    private readonly ImgurOptions _options;
+    private readonly ILogger<ImgurService> _logger;
+
+    public ImgurService(HttpClient client, IOptions<ImgurOptions> options, ILogger<ImgurService> logger)
+    {
+        _client = client;
+        _options = options.Value;
+        _logger = logger;
+    }
+
+    public async Task<List<string>> GetImagesAsync(string query)
+    {
+        _client.DefaultRequestHeaders.Add("Authorization", "Client-ID " + _options.ClientId);
+        var response = await _client.GetAsync($"https://api.imgur.com/3/gallery/search?q={query}");
+        response.EnsureSuccessStatusCode();
+
+        var responseJson = await response.Content.ReadAsStringAsync();
+
+        // Log the response
+        _logger.LogInformation("Imgur API response: {Response}", responseJson);
+
+        var responseObject = JsonDocument.Parse(responseJson);
+
+        var imageUrls = new List<string>();
+        foreach (var item in responseObject.RootElement.GetProperty("data").EnumerateArray())
+        {
+            if (item.TryGetProperty("images", out var images))
+            {
+                foreach (var image in images.EnumerateArray())
+                {
+                    if (image.TryGetProperty("link", out var linkProperty))
+                    {
+                        imageUrls.Add(linkProperty.GetString());
+                    }
+                }
+            }
+        }
+
+        return imageUrls;
+    }
+
+
+
+}
